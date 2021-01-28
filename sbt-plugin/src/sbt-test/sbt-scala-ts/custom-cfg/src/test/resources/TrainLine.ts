54ba9eda3bf74a980ad3326d47a09631026d1cfa
Cédric Chantepie <cchantep@users.noreply.github.com>
2021-01-28 21:47:11 +0100

    Failing test with field declared as Enumeration

--- sbt-plugin/src/sbt-test/sbt-scala-ts/custom-cfg/src/test/resources/TrainLine.ts
+++ sbt-plugin/src/sbt-test/sbt-scala-ts/custom-cfg/src/test/resources/TrainLine.ts
@@ -1,7 +1,9 @@
+import type { TSWeekDay } from './scalatsWeekDay';
 import type { TSTransport } from './scalatsTransport';
 
 export interface TSTrainLine extends TSTransport {
   _name: string;
   _startStationId: string;
   _endStationId: string;
+  _serviceDays: ReadonlyArray<TSWeekDay>;
 }
