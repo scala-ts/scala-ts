c3a6cad180dea35411ce14b2286955b33f5466d6
Cédric Chantepie <cchantep@users.noreply.github.com>
2021-03-20 17:05:12 +0100

    Add SBT scripted test for multi-project build (#126)

---
+++ sbt-plugin/src/sbt-test/sbt-scala-ts/multi/api/src/main/scala/CaseClassBar.scala
@@ -0,0 +1,5 @@
+package io.github.scalats.sbttest
+
+case class CaseClassBar(firstName: String, lastName: String)
+
+final class Grade(val value: Int) extends AnyVal
---
+++ sbt-plugin/src/sbt-test/sbt-scala-ts/multi/api/src/main/scala/CaseClassFoo.scala
@@ -0,0 +1,20 @@
+package io.github.scalats.sbttest
+
+import java.time._
+
+case class CaseClassFoo(
+  id: java.util.UUID,
+  name: String,
+  i: Int,
+  flag: Byte,
+  score: Short,
+  time: Long,
+  localDate: LocalDate,
+  instant: Instant,
+  localDateTime: LocalDateTime,
+  offsetDateTime: OffsetDateTime,
+  zonedDateTime: ZonedDateTime,
+  ts: java.sql.Timestamp,
+  tuple2: (String, Int),
+  tuple3: (Instant, LocalDate, CaseClassFoo),
+  bar: Option[CaseClassBar])
