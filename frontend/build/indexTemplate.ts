f9dd9ec1fa6973f8e1777e58d90f8a532aee8914
Cédric Chantepie <cchantep@users.noreply.github.com>
2022-05-29 22:11:10 +0200

    Update build (#493)
    * Close #490 - Update scalafmt-core to 3.5.7 in demo/akka-idtlt

--- frontend/build/indexTemplate.ts
+++ frontend/build/indexTemplate.ts
@@ -37,6 +37,7 @@ export function indexTemplate(options: Options) {
   </head>
 
   <body>
+    <script>var exports={}</script>
     ${script}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
