4c7bed402e2cfca02a3d2f519f291ef72d664bd1
Cédric Chantepie <cchantep@users.noreply.github.com>
2025-11-26 14:14:36 +0100

    Add meta on validator for non empty array

--- idtlt/src/test/scala/IdtltTypeMapperSpec.scala
+++ idtlt/src/test/scala/IdtltTypeMapperSpec.scala
@@ -24,7 +24,7 @@ final class IdtltTypeMapperSpec extends org.specs2.mutable.Specification {
 
     "support non empty array" in {
       mapType(ArrayRef(StringRef, true)) must beSome(
-        "idtlt.readonlyArray(idtlt.string).and(([head, ...tail]) => (head !== undefined) ? idtlt.Ok([head, ...tail] as const) : idtlt.Err('Invalid non empty array'))"
+        "(() => { const av = idtlt.readonlyArray(idtlt.string); av.meta.minLength = 1; av.and(([head, ...tail]) => (head !== undefined) ? idtlt.Ok([head, ...tail] as const) : idtlt.Err('Invalid non empty array')) })()"
       )
     }
   }
